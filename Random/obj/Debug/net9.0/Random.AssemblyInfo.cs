295ec6fd8321b83f8a9f3feafef74acca22e9130
1ap <koszutowski.bartek@gmail.com>
2025-01-28 14:47:21 +0100

    Api

--- Random/obj/Debug/net9.0/Random.AssemblyInfo.cs
+++ Random/obj/Debug/net9.0/Random.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Random")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb26eced4f55c246fdf71018ecf7822aa820d3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Random")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Random")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
